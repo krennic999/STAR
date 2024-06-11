c74bc2e706b8fce7d69173d21f8a790a10f2d8a1
Mohan Zhou <mhzhou99@outlook.com>
2024-06-11 15:19:17 +0800

    init

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "STAR",
+  description: "Scale-wise Text-to-image generation via Auto-Regressive representations",
+  icons: "/favicon.png",
+  authors: [{ name: "Mohan Zhou", url: "https://mhzhou.com" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
